a1c8c72decba8b23f25467c405ce4cbb90cb2a54
TiciaChance <laticia.chance@flatironschool.com>
2016-09-07 12:31:38 -0400

    first commit

--- GoTQuotes/ViewController.swift
+++ GoTQuotes/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var characterLabel: UILabel!
+    @IBOutlet weak var quoteView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        callQuotes()
+        
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        view.
+        
+    }
+    
+    func callQuotes() {
+        
+        Alamofire.request(.GET, "https://got-quotes.herokuapp.com/quotes?").responseJSON {(response)  in
+            
+            //print(response)
+            
+            
+            guard let jsonData = response.data else {return}
+            
+            let jsonObject = JSON(data: jsonData)
+            
+            guard let dictionaryOfData = jsonObject.dictionary else {return}
+            
+            guard let quote = dictionaryOfData["quote"]?.string else {return}
+            guard let character = dictionaryOfData["character"]?.string else {return}
+            
+            
+            self.quoteLabel.text = quote
+            self.characterLabel.text = character
+            self.characterLabel.textColor = UIColor.redColor()
+        }
+        
+    }
+    
+    func animateView() {
+        
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
---
+++ Pods/Target Support Files/Pods-GoTQuotes/Pods-GoTQuotes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoTQuotes : NSObject
+@end
+@implementation PodsDummy_Pods_GoTQuotes
+@end
