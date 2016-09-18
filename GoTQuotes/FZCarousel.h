36621e85c2869221b789b221532834220b91e8fe
TiciaChance <laticia.chance@flatironschool.com>
2016-09-18 12:02:56 -0400

    added carousel cocoapod and updated to swift 3

---
+++ GoTQuotes/FZCarousel.h
@@ -0,0 +1,49 @@
+//
+//  FZCarousel.h
+//  GoTQuotes
+//
+//  Created by Laticia Chance on 9/18/16.
+//  Copyright © 2016 Laticia Chance. All rights reserved.
+//
+
+#ifndef FZCarousel_h
+#define FZCarousel_h
+
+#import "FZCarouselView.h"
+#endif /* FZCarousel_h */
+
+//
+//  FZCarouselView.h
+//  FZCarousel
+//
+//  Created by Sheng Dong on 9/25/15.
+//  Copyright © 2015 Fuzz Productions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FZCarouselCollectionViewDelegate.h"
+
+
+@interface FZDefaultCarouselCollectionViewDelegate : FZCarouselCollectionViewDelegate
+@end
+
+/**
+ * @description FZCarouselView is view with default implementation of FZCarouselCollectionViewDelegate that takes an array of images for an infinitely scrolling carousel.
+ *
+ * @discussion Use it like a standard view with or without storyboard.
+ */
+
+@interface FZCarouselView : UIView
+
+@property (readonly, nonatomic) FZDefaultCarouselCollectionViewDelegate *carouselCollectionViewDelegate;
+@property (readonly, nonatomic) UICollectionView *collectionView;
+
+
+@property (nonatomic, strong) NSArray<UIImage *> *imageArray;
+@property (nonatomic) BOOL gestureRecognitionShouldEndCarousel;
+@property (nonatomic, assign) NSTimeInterval crankInterval; // Three Seconds by default
+
+- (void)beginCarousel;
+
+@end
